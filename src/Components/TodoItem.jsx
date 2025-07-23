4a13afa4306e9b5ec1f9d62be5a20fd5f309cc0b
Uday bandari <udaybandari593@gmail.com>
2025-07-23 17:30:17 +0530

    added new feature

--- src/Components/TodoItem.jsx
+++ src/Components/TodoItem.jsx
@@ -6,13 +6,23 @@ import { MdOutlineRadioButtonUnchecked } from "react-icons/md";
 import { useState } from "react";
 
 const TodoItem = ({tasks,setTasks,filter}) => {
+  const [editId, setEditId] = useState(null);
+const [editText, setEditText] = useState("");
+
     const handletoggle=(id)=>{
     setTasks(prev=>
          prev.map((t)=>t.id===id?{...t,completed:!t.completed}:t)
 
 
     );
+  
+} 
+const handlechange=(id)=>{
+  setTasks((prev)=>prev.map((a)=>a.id==id?{...a,text:editText}:a));
+  setEditId(null);
+  setEditText("");
 }
+console.log(editText)
 const filteredTasks = tasks.filter(task => {
   if (filter === "completed") return task.completed;
   if (filter === "active") return !task.completed;
@@ -30,8 +40,20 @@ const filteredTasks = tasks.filter(task => {
   {task.completed ? <IoMdCheckmarkCircle /> : <MdOutlineRadioButtonUnchecked />}
 
 </button>
-
-       <h1 className="text-4xl max-md:text-2xl  max-lg:text-2xl"> {task.text}</h1>
+        
+   {editId==task.id?(
+        <>
+        <input
+         type="text" 
+         value={editText}
+         onChange={(e)=>setEditText(e.target.value)} 
+         className="rounded-md w-40 font-semibold"
+         autoFocus
+         />
+         <button onClick={()=>handlechange(editId)} className="w-12 cursor-pointer text-sm border-1 rounded-md bg-gradient-to-r from-blue-400 to-blue-800 text-white font-semibold ">save </button>
+        </>
+   ):(    <h1 className="text-4xl font-serif max-md:text-2xl  max-lg:text-2xl"> {task.text}</h1>)}
+       <button  className='cursor-pointer absolute right-13  text-2xl top-4' onClick={()=>{setEditId(task.id),setEditText(task.text)}} >✏️</button>
        <button  onClick={()=>setTasks(prev => prev.filter(t => t.id !==task.id))}>
        <RxCross2 className="text-4xl  max-md:text-2xl  max-md:right-4 max-md:top-5 absolute right-0 top-3 text-red-500 font-bold  cursor-pointer "  />
        </button>
